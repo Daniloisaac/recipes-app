73424997a4f3466c0322cf71fd691a4c71813b52
Danilo Isaac <daniloisaac942@gmail.com>
2022-09-23 17:45:36 -0300

    adicionei o menu inferior finalizado, sujeito a mudanças. projeto em andamento

--- src/tests/Footer.test.js
+++ src/tests/Footer.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from '../App';
+import { screen } from '@testing-library/react';
+// import App from '../App';
+import Drinks from '../pages/Drinks';
+import renderWithRouter from './helpers/renderWithRouter';
 
 describe('testando o componente "Footer"', () => {
-  test('Farewell, front-end', () => {
-    render(<App />);
+  test('testando se as imagens de drink e meals estão na tela', () => {
+    renderWithRouter(<Drinks />);
+
+    const imgDrink = screen.getByTestId('drinks-bottom-btn');
+    const imgMeals = screen.getByTestId('meals-bottom-btn');
+
+    expect(imgDrink).toBeInTheDocument();
+    expect(imgMeals).toBeInTheDocument();
   });
 });
