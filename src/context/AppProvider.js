0774ba61322ad0942363d7256a44529f3c07625e
Danilo Isaac <daniloisaac942@gmail.com>
2022-09-23 19:12:39 -0300

    adicionei o requisito 2 da tela de login

--- src/context/AppProvider.js
+++ src/context/AppProvider.js
@@ -3,28 +3,19 @@ import PropTypes from 'prop-types';
 import AppContext from './AppContext';
 
 function AppProvider({ children }) {
-  // const [stateEmail, setStateEmail] = useState('');
-  // const [ statePassw , setStatePassw ] = useState('');
-  // const [stateDisable, setStateEnable] = useState(false);
-  // const contextValue = {
-  // stateEmail,
-  // setStateEmail,
-  // statePassw,
-  // setStatePassw,
-  // stateDisable,
-  // setStateEnable,
-  // };
   const contextValue = 'teste';
 
   return (
-    <AppContext.Provider value={ contextValue }>
-      {children}
-    </AppContext.Provider>
+    <div>
+      <AppContext.Provider value={ contextValue }>
+        {children}
+      </AppContext.Provider>
+    </div>
   );
 }
 
 AppProvider.propTypes = {
-  children: PropTypes.node,
-}.isRequired;
+  children: PropTypes.node.isRequired,
+};
 
 export default AppProvider;
