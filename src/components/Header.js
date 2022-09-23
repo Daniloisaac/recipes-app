94b08f29caa1cc670aa8c7668b312810ced26319
Eric <ericdasilva0@gmail.com>
2022-09-23 16:11:18 -0300

    context confi

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 function Header() {
   return (
     <header>
-      <button>
-        <img 
-          data-testid="profile-top-btn"
-        />
-      </button>
-
+      <h1>olá</h1>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
