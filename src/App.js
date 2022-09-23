0b727467aa9697a4601af4d2961fac6d11b58f0e
Eric <ericdasilva0@gmail.com>
2022-09-23 18:07:41 -0300

    att

--- src/App.js
+++ src/App.js
@@ -1,36 +1,25 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Footer from './components/Footer';
 import AppProvider from './context/AppProvider';
 
 function App() {
   return (
-<<<<<<< HEAD
     <AppProvider>
-      <Switch>
-        <Route exact path="/" component={ Login } />
-        <Route exact path="/meals" component={ Meals } />
-        <Route exact path="/drinks" component={ Drinks } />
-        <Route exact path="/profile" component={ Profile } />
-        <Route exact path="done-recipes" component={ DoneRecipes } />
-        <Route exact path="favorite-recipes" component={ FavoriteRecipes } />
-        <Footer />
-      </Switch>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route exact path="/meals" component={ Meals } />
+          <Route exact path="/drinks" component={ Drinks } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route exact path="done-recipes" component={ DoneRecipes } />
+          <Route exact path="favorite-recipes" component={ FavoriteRecipes } />
+          <Footer />
+        </Switch>
+      </BrowserRouter>
     </AppProvider>
-=======
-    <div className="meals">
-      <span className="logo">TRYBE 2.0</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
->>>>>>> origin
   );
 }
 
