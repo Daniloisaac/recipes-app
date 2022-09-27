1c389468c4046fa3a40560afa00159382a3a5c2d
pedroygor <pedroygor2010@hotmail.com>
2022-09-27 05:39:13 -0300

    erro no index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <App />,
+  document.getElementById('root'),
 );
