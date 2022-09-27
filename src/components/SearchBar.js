f385be093014066f6e16bdabb8548f71e27fc14f
pedroygor <pedroygor2010@hotmail.com>
2022-09-27 16:33:51 -0300

    requisito 10

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,25 +4,35 @@ function SearchBar() {
       <input
         type="text"
         data-testid="search-input"
+        placeholder="busca"
       />
-      <input
-        type="radio"
-        id="input_ingredient"
-        name="ingredient"
-        data-testid="ingredient-search-radio"
-      />
-      <input
-        type="radio"
-        id="input_name"
-        name="name"
-        data-testid="name-search-radio"
-      />
-      <input
-        type="radio"
-        id="input_firstLetter"
-        name="first_letter"
-        data-testid="first-letter-search-radio"
-      />
+      <label htmlFor="input_ingredient">
+        <input
+          type="radio"
+          id="input_ingredient"
+          name="ingredient"
+          data-testid="ingredient-search-radio"
+        />
+        ingredient
+      </label>
+      <label htmlFor="input_name">
+        <input
+          type="radio"
+          id="input_name"
+          name="name"
+          data-testid="name-search-radio"
+        />
+        Name
+      </label>
+      <label htmlFor="input_firstLetter">
+        <input
+          type="radio"
+          id="input_firstLetter"
+          name="first_letter"
+          data-testid="first-letter-search-radio"
+        />
+        First Letter
+      </label>
       <button
         type="button"
         data-testid="exec-search-btn"
