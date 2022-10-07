ef7af8b421865b0bde867b4830c15fbfb3336f9a
ana-beatriz-martins-lima <ana.martins1912@gmail.com>
2022-10-07 14:23:18 -0300

    linter

--- src/tests/DoneRecipes.test.js
+++ src/tests/DoneRecipes.test.js
@@ -1,7 +1,3 @@
-import React from 'react';
-import { screen, waitFor } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-
 describe('testando a page Dono Recipes', () => {
-  
+  // testes
 });
