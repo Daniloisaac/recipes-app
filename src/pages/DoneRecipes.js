2a90e9944816dbc9c23e3f0dd334ce52952a8dba
Danilo Isaac <daniloisaac942@gmail.com>
2022-10-07 20:56:32 -0300

    .

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -79,7 +79,7 @@ function DoneRecipes() {
           onClick={ handleFilter }
           data-testid="filter-by-meal-btn"
         >
-          Meals
+          Mealss
         </button>
         <button
           type="button"
